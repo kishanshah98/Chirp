75806369ab7942b08f3d547873573379afeb872b
Michael <kiliscout1010@gmail.com>
2022-07-06 15:10:53 -0400

    debugging sigin

--- server.js
+++ server.js
@@ -31,6 +31,9 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+console.log(path.join(__dirname, 'public'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
   console.log(`${req.method} requested on endpoint ${req.path}`)
